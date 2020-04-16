5d4800dddaf038ae7ae6709ed75d7e9f0228906c
Lucas Albuquerque <lucas.albuquerque.felipini@gmail.com>
2020-04-16 20:41:58 -0300

    First commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+    for (let i = 0; i < this.transactions.length; i += 1) {
+      if (this.transactions[i].type === 'income') {
+        income += this.transactions[i].value;
+      } else {
+        outcome += this.transactions[i].value;
+      }
+    }
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    if (type === 'outcome') {
+      const { total } = this.getBalance();
+      if (total - value < 0) {
+        throw Error('user cannot spend more value than available');
+      }
+    }
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
